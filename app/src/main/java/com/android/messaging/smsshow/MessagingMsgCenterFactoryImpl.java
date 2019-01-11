123315e16766eac9bffcb0e78aa7da40c4f232c6
dong.guo <dong.guo@ihandysoft.com>
2019-01-11 17:41:33 +0800

    fix sms show switch error

--- app/src/main/java/com/android/messaging/smsshow/MessagingMsgCenterFactoryImpl.java
+++ app/src/main/java/com/android/messaging/smsshow/MessagingMsgCenterFactoryImpl.java
@@ -6,6 +6,7 @@ import com.android.messaging.ui.UIIntents;
 import com.android.messaging.util.PhoneUtils;
 import com.messagecenter.customize.MessageCenterFactoryImpl;
 import com.messagecenter.customize.SmsShowCallBack;
+import com.messagecenter.notification.NotificationMessageAlertActivity;
 import com.messagecenter.sms.SmsMessageAlertActivity;
 
 public class MessagingMsgCenterFactoryImpl extends MessageCenterFactoryImpl {
@@ -56,9 +57,29 @@ public class MessagingMsgCenterFactoryImpl extends MessageCenterFactoryImpl {
 
             @Override
             public void onMessageBoxPrepareToStart() {
-                if (SmsShowUtils.getSmsShowAppliedId() > 0) {
+                if (SmsShowUtils.getSmsShowAppliedId() > 0 && SmsShowUtils.isSmsShowEnabledByUser()) {
                     UIIntents.get().launchSmsShowActivity();
                 }
+            }
+        };
+    }
+
+    @Override
+    public NotificationMessageAlertActivity.Config getNotificationMessageConfig() {
+        return new NotificationMessageAlertActivity.Config() {
+            @Override
+            public String getAdPlacement() {
+                return null;
+            }
+
+            @Override
+            public boolean enable() {
+                return false;
+            }
+
+            @Override
+            public boolean showDefaultSmsAppEntranceIcon() {
+                return false;
             }
         };
     }
