65db612110ff12a07e7af4c2fd40ef2203c27970
dong.guo <dong.guo@ihandysoft.com>
2019-02-28 21:14:20 +0800

    [Build] add logs

--- app/src/main/java/com/android/messaging/ui/wallpaper/WallpaperEditActivity.java
+++ app/src/main/java/com/android/messaging/ui/wallpaper/WallpaperEditActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -133,7 +134,7 @@ public class WallpaperEditActivity extends HSAppCompatActivity implements View.O
     }
 
     private void errorState(String msg) {
-        HSLog.w(msg);
+        Log.w(TAG, msg);
         finish();
     }
 
