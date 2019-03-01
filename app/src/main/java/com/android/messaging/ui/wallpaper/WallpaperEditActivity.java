4f6e3910a449d289e04b47fc05bd68647f47bbe0
dong.guo <dong.guo@ihandysoft.com>
2019-03-01 10:59:29 +0800

    remove class Log

--- app/src/main/java/com/android/messaging/ui/wallpaper/WallpaperEditActivity.java
+++ app/src/main/java/com/android/messaging/ui/wallpaper/WallpaperEditActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -134,7 +133,7 @@ public class WallpaperEditActivity extends HSAppCompatActivity implements View.O
     }
 
     private void errorState(String msg) {
-        Log.w(TAG, msg);
+        HSLog.w(TAG, msg);
         finish();
     }
 
