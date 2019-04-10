f4056db177f684fd93fa02ba231aa8c10d6c576b
Tian Tian <tian.tian@ihandysoft.com>
2019-04-10 16:59:17 +0800

    [Build]

--- app/src/main/java/com/android/messaging/ui/messagebox/MessageBoxInputActionView.java
+++ app/src/main/java/com/android/messaging/ui/messagebox/MessageBoxInputActionView.java
@@ -62,6 +62,7 @@ class MessageBoxInputActionView extends LinearLayout {
             sb.setSpan(signatureSpan, 1, sb.length(), 0);
             sb.setSpan(new AbsoluteSizeSpan(13, true), 1, sb.length(), 0);
             mComposeEditText.setText(sb, TextView.BufferType.SPANNABLE);
+            mComposeEditText.setSelection(0);
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             mComposeEditText.setShowSoftInputOnFocus(false);
