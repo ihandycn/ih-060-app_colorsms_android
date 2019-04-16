f5f0e27f8b2e5d23b013bf549a4c3b39d29b4b8a
zheli1 <zhe.li.1@ihandysoft.com>
2019-04-16 13:28:45 +0800

    fix ad logics

--- app/src/main/java/com/android/messaging/ui/conversationlist/ConversationListFragment.java
+++ app/src/main/java/com/android/messaging/ui/conversationlist/ConversationListFragment.java
@@ -358,12 +358,15 @@ public class ConversationListFragment extends Fragment implements ConversationLi
                 } catch (Exception e) {
                 }
 
-                Threads.postOnMainThread(() -> {
-                    TextView title = expressAdView.findViewById(R.id.banner_title);
-                    title.setTextColor(ConversationColors.get().getListTitleColor());
-                    TextView subtitle = expressAdView.findViewById(R.id.banner_des);
-                    subtitle.setTextColor(ConversationColors.get().getListSubtitleColor());
-                });
+                try {
+                    Threads.postOnMainThread(() -> {
+                        TextView title = expressAdView.findViewById(R.id.banner_title);
+                        title.setTextColor(ConversationColors.get().getListTitleColor());
+                        TextView subtitle = expressAdView.findViewById(R.id.banner_des);
+                        subtitle.setTextColor(ConversationColors.get().getListSubtitleColor());
+                    });
+                } catch (Exception e) {
+                }
             }
 
             @Override public void onChildViewRemoved(View parent, View child) {
