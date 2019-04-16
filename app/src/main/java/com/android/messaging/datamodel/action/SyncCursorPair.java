286338dd4d6cf385d4a0a66ed7112cf34d7613dd
fengjin <jin.feng@ihandysoft.com>
2019-04-16 12:09:41 +0800

    filter the "sms_message_uri" column in table "messages" to make sure the private messages not be synced

--- app/src/main/java/com/android/messaging/datamodel/action/SyncCursorPair.java
+++ app/src/main/java/com/android/messaging/datamodel/action/SyncCursorPair.java
@@ -32,6 +32,7 @@ import com.android.messaging.datamodel.SyncManager;
 import com.android.messaging.datamodel.SyncManager.ThreadInfoCache;
 import com.android.messaging.datamodel.data.MessageData;
 import com.android.messaging.mmslib.SqliteWrapper;
+import com.android.messaging.privatebox.PrivateMessageContentProvider;
 import com.android.messaging.privatebox.PrivateMessageManager;
 import com.android.messaging.sms.DatabaseMessages;
 import com.android.messaging.sms.DatabaseMessages.DatabaseMessage;
@@ -327,9 +328,13 @@ class SyncCursorPair {
     private static final String ORDER_BY_DATE_DESC = "date DESC";
 
     // A subquery that selects SMS/MMS messages in Bugle which are also in telephony
+    // for private box :filter the "sms_message_uri" column
+    // where not like content://com.color.sms.messages
+    // to make sure the private messages not be synced
     private static final String LOCAL_MESSAGES_SELECTION = String.format(
             Locale.US,
-            "(%s NOTNULL)",
+            "(%s NOTNULL) AND " + MessageColumns.SMS_MESSAGE_URI + " NOT LIKE '"
+                    + PrivateMessageContentProvider.BASE_CONTENT_URI.toString() + "%%' ",
             MessageColumns.SMS_MESSAGE_URI);
 
     private static final String ORDER_BY_TIMESTAMP_DESC =
