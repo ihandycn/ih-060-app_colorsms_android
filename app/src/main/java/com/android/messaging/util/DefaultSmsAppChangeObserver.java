f19195d1f9c05e0d063404e6ec24346d62a80625
fengjin <jin.feng@ihandysoft.com>
2019-05-30 16:44:23 +0800

    try to fix https://fabric.io/smsgroup/android/apps/com.color.sms.messages.emoji/issues/5c745754f8b88c29631d4a06

--- app/src/main/java/com/android/messaging/util/DefaultSmsAppChangeObserver.java
+++ app/src/main/java/com/android/messaging/util/DefaultSmsAppChangeObserver.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import com.android.messaging.BuildConfig;
 import com.android.messaging.Factory;
 import com.android.messaging.ui.SetAsDefaultGuideActivity;
+import com.ihs.app.framework.HSApplication;
 import com.ihs.commons.config.HSConfig;
 import com.superapps.util.Threads;
 import com.superapps.util.Toasts;
@@ -28,7 +29,7 @@ public class DefaultSmsAppChangeObserver extends ContentObserver {
             public void run() {
                 if (!PhoneUtils.getDefault().isDefaultSmsApp()) {
                     if (HSConfig.optBoolean(false, "Application", "SetDefaultAlert", "Switch")) {
-                        SetAsDefaultGuideActivity.startActivity(Factory.get().getApplicationContext(), SetAsDefaultGuideActivity.DEFAULT_CHANGED);
+                        SetAsDefaultGuideActivity.startActivity(HSApplication.getContext(), SetAsDefaultGuideActivity.DEFAULT_CHANGED);
                     }
                 }
 
