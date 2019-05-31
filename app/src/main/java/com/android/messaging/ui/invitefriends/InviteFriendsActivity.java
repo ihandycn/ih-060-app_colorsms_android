251115efe2928f81e43b7e57619b83c75543ac9f
dong.guo <dong.guo@ihandysoft.com>
2019-05-31 15:09:10 +0800

    add layout invite friends

--- app/src/main/java/com/android/messaging/ui/invitefriends/InviteFriendsActivity.java
+++ app/src/main/java/com/android/messaging/ui/invitefriends/InviteFriendsActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.android.messaging.R;
+
 public class InviteFriendsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_invite_friends);
     }
 
 }
