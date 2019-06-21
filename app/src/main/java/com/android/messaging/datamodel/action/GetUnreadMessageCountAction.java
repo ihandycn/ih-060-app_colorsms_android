2ad9b78b116cf1ff31ab1f5980bd6e922d680107
fengjin <jin.feng@ihandysoft.com>
2019-06-21 17:01:42 +0800

    fix issue #210

--- app/src/main/java/com/android/messaging/datamodel/action/GetUnreadMessageCountAction.java
+++ app/src/main/java/com/android/messaging/datamodel/action/GetUnreadMessageCountAction.java
@@ -29,10 +29,15 @@ public class GetUnreadMessageCountAction extends Action implements Parcelable {
         // Update local db
         db.beginTransaction();
         try {
-            final Cursor cursor = db.query(DatabaseHelper.MESSAGES_TABLE, new String[]{MessageColumns.READ},
-                    MessageColumns.STATUS + "=\"" + BUGLE_STATUS_INCOMING_COMPLETE + "\" AND "
-                            + MessageColumns.READ + "!=?", new String[]{"1"},
-                    null, null, null, null);
+            final Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM "
+                            + DatabaseHelper.MESSAGES_TABLE + " , "
+                            + DatabaseHelper.PARTICIPANTS_TABLE
+                            + " WHERE " + DatabaseHelper.MESSAGES_TABLE + "." + MessageColumns.SENDER_PARTICIPANT_ID
+                            + "=" + DatabaseHelper.PARTICIPANTS_TABLE + "." + DatabaseHelper.ParticipantColumns._ID
+                            + " AND " + MessageColumns.STATUS + "=\"" + BUGLE_STATUS_INCOMING_COMPLETE + "\" "
+                            + " AND " + MessageColumns.READ + " = 0 "
+                            + " AND " + DatabaseHelper.ParticipantColumns.BLOCKED + " != 0"
+                    , null);
             UnreadMessageManager.getInstance().setUnreadMessageCount(cursor.getCount());
             cursor.close();
             db.setTransactionSuccessful();
