1812b0cf967f5b1529ebbc8610176bf4e1136c64
dong.guo <dong.guo@ihandysoft.com>
2019-06-24 20:15:40 +0800

    use bigger avatar bg in long screen device

--- app/src/main/java/com/android/messaging/ui/customize/AvatarBgDrawables.java
+++ app/src/main/java/com/android/messaging/ui/customize/AvatarBgDrawables.java
@@ -5,11 +5,14 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
+import com.android.messaging.R;
 import com.android.messaging.ui.customize.theme.ThemeInfo;
 import com.android.messaging.ui.customize.theme.ThemeManager;
 import com.android.messaging.ui.customize.theme.ThemeUtils;
 import com.android.messaging.util.CommonUtils;
+import com.android.messaging.util.UiUtils;
 import com.ihs.app.framework.HSApplication;
+import com.ihs.commons.utils.HSLog;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,7 +29,10 @@ public class AvatarBgDrawables {
         ThemeInfo info = ThemeUtils.getCurrentTheme();
 
         if (info.mThemeKey.equals(ThemeUtils.DEFAULT_THEME_KEY)) {
-
+            if (UiUtils.isLongScreenDevice(HSApplication.getContext())) {
+                sAvatarBg = HSApplication.getContext().getResources().getDrawable(R.drawable.default_theme_avatar_long_device_bg);
+                return sAvatarBg;
+            }
         }
 
         if (!info.isInLocalFolder()) {
