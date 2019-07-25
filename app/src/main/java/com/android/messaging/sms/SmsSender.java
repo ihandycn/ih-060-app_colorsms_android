2f62fdfa8891c99cc30d33815ec226321a04c2fc
fengjin <jin.feng@ihandysoft.com>
2019-07-25 15:31:27 +0800

    fix NPE

--- app/src/main/java/com/android/messaging/sms/SmsSender.java
+++ app/src/main/java/com/android/messaging/sms/SmsSender.java
@@ -149,7 +149,8 @@ public class SmsSender {
             BugleAnalytics.logEvent("SMS_Send_Failed",
                     "SmsSendResult", "False", "FailCarrierName", carrierName);
             BugleFirebaseAnalytics.logEvent("SMS_Send_Failed",
-                    "SmsSendResult", "False", "FailCarrierName", carrierName.length() < 40 ? carrierName : carrierName.substring(0, 39));
+                    "SmsSendResult", "False",
+                    "FailCarrierName", (carrierName != null && carrierName.length() < 40) ? carrierName : carrierName.substring(0, 39));
             if (errorCode != SendStatusReceiver.NO_ERROR_CODE) {
                 final Context context = Factory.get().getApplicationContext();
                 UiUtils.showToastAtBottom(getSendErrorToastMessage(context, subId, errorCode));
