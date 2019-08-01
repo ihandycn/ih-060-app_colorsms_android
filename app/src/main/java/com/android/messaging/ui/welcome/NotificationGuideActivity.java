811b843d626821fbb85d3bb5dcb37ed38c8865e3
张天亮 <tianliang.zhang@ihandysoft.com>
2019-08-01 20:45:55 +0800

    Add notification access page key pressed logic.

--- app/src/main/java/com/android/messaging/ui/welcome/NotificationGuideActivity.java
+++ app/src/main/java/com/android/messaging/ui/welcome/NotificationGuideActivity.java
@@ -1,9 +1,12 @@
 package com.android.messaging.ui.welcome;
 
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.view.animation.PathInterpolatorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,15 +16,45 @@ import android.widget.TextView;
 
 import com.android.messaging.R;
 import com.android.messaging.ui.UIIntents;
+import com.android.messaging.ui.conversationlist.ConversationListActivity;
 import com.android.messaging.ui.dialog.PermissionGuideManager;
 import com.android.messaging.util.NotificationCleanerUtils;
 import com.ihs.app.framework.HSApplication;
-import com.ihs.commons.utils.HSLog;
 import com.superapps.util.BackgroundDrawables;
 import com.superapps.util.Dimensions;
 import com.superapps.util.Threads;
 
 public class NotificationGuideActivity extends AppCompatActivity {
+    public static final int MSG_WHAT_NOTIFICATION_LISTENING_CHECK = 100;
+    public static final int MSG_WHAT_NOTIFICATION_LISTENING_CANCEL = 101;
+    public static final int INTERVAL_PERMISSION_CHECK = 1000;
+    public static final int DELAY_START_TO_PERMISSION_CHECK = 1000;
+    public static final int DURATION_PERMISSION_CHECK_CONTINUED = 120000;
+    private boolean mIsBackPressedFromNotificationAccessSettings;
+
+    @SuppressLint("HandlerLeak") // This handler holds activity reference for no longer than 120s
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_WHAT_NOTIFICATION_LISTENING_CHECK:
+                    if (!NotificationCleanerUtils.isNotificationAccessGranted(NotificationGuideActivity.this)) {
+                        sendEmptyMessageDelayed(MSG_WHAT_NOTIFICATION_LISTENING_CHECK, INTERVAL_PERMISSION_CHECK);
+                        break;
+                    }
+                    PermissionGuideManager.getInstance().removePermissionGuide(false);
+                    Intent intentSelf = new Intent(HSApplication.getContext(), NotificationGuideActivity.class);
+                    intentSelf.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    HSApplication.getContext().startActivity(intentSelf);
+                    break;
+                case MSG_WHAT_NOTIFICATION_LISTENING_CANCEL:
+                    removeMessages(MSG_WHAT_NOTIFICATION_LISTENING_CHECK);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,16 +110,15 @@ public class NotificationGuideActivity extends AppCompatActivity {
         });
 
         confirmButton.setOnClickListener(v -> {
-            try {
-                Intent intent = new Intent(NotificationCleanerUtils.ACTION_NOTIFICATION_LISTENER_SETTINGS);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                finish();
-            } catch (Exception e) {
-                HSLog.d("Notification Test", "Start system setting error!");
-            }
+            Intent intent = new Intent(NotificationCleanerUtils.ACTION_NOTIFICATION_LISTENER_SETTINGS);
+            startActivity(intent);
             PermissionGuideManager.getInstance()
-                    .showPermissionGuide(HSApplication.getContext(), PermissionGuideManager.PermissionGuideType.NOTIFICATION_CLEANER_ACCESS_FULL_SCREEN, false);
+                    .showPermissionGuide(HSApplication.getContext(),
+                            PermissionGuideManager.PermissionGuideType.NOTIFICATION_CLEANER_ACCESS_FULL_SCREEN, false);
+            mIsBackPressedFromNotificationAccessSettings = true;
+            handler.removeMessages(MSG_WHAT_NOTIFICATION_LISTENING_CHECK);
+            handler.sendEmptyMessageDelayed(MSG_WHAT_NOTIFICATION_LISTENING_CHECK, DELAY_START_TO_PERMISSION_CHECK);
+            handler.sendEmptyMessageDelayed(MSG_WHAT_NOTIFICATION_LISTENING_CANCEL, DURATION_PERMISSION_CHECK_CONTINUED);
         });
     }
 
@@ -94,5 +126,24 @@ public class NotificationGuideActivity extends AppCompatActivity {
     public void onBackPressed() {
         UIIntents.get().launchConversationListActivity(NotificationGuideActivity.this);
         super.onBackPressed();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!NotificationCleanerUtils.isNotificationAccessGranted(NotificationGuideActivity.this) && !mIsBackPressedFromNotificationAccessSettings) {
+            return;
+        }
+        Intent intent = new Intent(NotificationGuideActivity.this, ConversationListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeMessages(MSG_WHAT_NOTIFICATION_LISTENING_CANCEL);
+        handler.removeMessages(MSG_WHAT_NOTIFICATION_LISTENING_CHECK);
     }
 }
--- app/src/main/java/com/android/messaging/util/NotificationCleanerUtils.java
+++ app/src/main/java/com/android/messaging/util/NotificationCleanerUtils.java
@@ -2,30 +2,13 @@ package com.android.messaging.util;
 
 import android.content.ComponentName;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.PixelFormat;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
 public class NotificationCleanerUtils {
 
     private static final String ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners";
-    private static final String PREF_KEY_LAUNCHER_SETTINGS_NOTIFICATION_OPENED = "PREF_KEY_LAUNCHER_SETTINGS_NOTIFICATION_OPENED";
-    private static final String PREF_KEY_NOTIFICATION_CLEANER_OPENED = "NOTIFICATION_CLEANER_OPENED";
-    private static final String PREF_KEY_JUNK_CLEANER_SETTINGS_NOTIFICATION_OPENED = "PREF_KEY_JUNK_CLEANER_SETTINGS_NOTIFICATION_OPENED";
-
     public static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
-
-    public static final String EXTRA_IS_AUTHORIZATION_SUCCESS = "EXTRA_IS_AUTHORIZATION_SUCCESS";
-
-    public static final String PREF_KEY_NOTIFICATION_BLOCKED_ACTIVITY_FAKE_INFO_ON_SHOW = "PREF_KEY_NOTIFICATION_BLOCKED_ACTIVITY_FAKE_INFO_ON_SHOW";
-    public static final String PREF_KEY_NOTIFICATION_BLOCKED_ACTIVITY_FAKE_INFO_LOGGED = "PREF_KEY_NOTIFICATION_BLOCKED_ACTIVITY_FAKE_INFO_LOGGED";
-
-    public static final String PREF_KEY_NOTIFICATION_CLEANER_SETTINGS_EVER_SWITCHED = "PREF_KEY_NOTIFICATION_CLEANER_SETTINGS_EVER_SWITCHED";
 
     public static boolean isNotificationAccessGranted(Context context) {
         final String flat = Settings.Secure.getString(context.getContentResolver(), ENABLED_NOTIFICATION_LISTENERS);
@@ -45,22 +28,5 @@ public class NotificationCleanerUtils {
         }
 
         return false;
-    }
-
-    public static Bitmap drawableToBitmap(@NonNull Drawable drawable) {
-        if (drawable instanceof BitmapDrawable) {
-            return ((BitmapDrawable) drawable).getBitmap();
-        }
-
-        final int width = drawable.getIntrinsicWidth();
-        final int height = drawable.getIntrinsicHeight();
-
-        Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565;
-        Bitmap bitmap = Bitmap.createBitmap(width, height, config);
-
-        drawable.setBounds(0, 0, width, height);
-        drawable.draw(new Canvas(bitmap));
-
-        return bitmap;
     }
 }
