a8a03b3545f97130b0f7783003081480b3ab9eec
fengjin <jin.feng@ihandysoft.com>
2019-08-06 11:08:16 +0800

    add some resources

---
+++ app/src/main/java/com/android/messaging/scheduledsms/TimePickerDialogWithButtonEvent.java
@@ -0,0 +1,35 @@
+package com.android.messaging.scheduledsms;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class TimePickerDialogWithButtonEvent extends TimePickerDialog {
+    private OnClickListener mPositiveButtonListener;
+    private OnClickListener mNegativeButtonListener;
+
+    public TimePickerDialogWithButtonEvent(Context context, OnTimeSetListener listener, int hourOfDay, int minute, boolean is24HourView) {
+        super(context, listener, hourOfDay, minute, is24HourView);
+    }
+
+    @Override
+    public void setButton(int whichButton, CharSequence text, OnClickListener listener) {
+        super.setButton(whichButton, text, this);
+        if (whichButton == DialogInterface.BUTTON_POSITIVE) {
+            mPositiveButtonListener = listener;
+        } else if (whichButton == DialogInterface.BUTTON_NEGATIVE) {
+            mNegativeButtonListener = listener;
+        }
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        super.onClick(dialog, which);
+        if (mPositiveButtonListener != null && which == DialogInterface.BUTTON_POSITIVE) {
+            mPositiveButtonListener.onClick(this, DialogInterface.BUTTON_POSITIVE);
+        }
+        if (mNegativeButtonListener != null && which == DialogInterface.BUTTON_NEGATIVE) {
+            mNegativeButtonListener.onClick(this, DialogInterface.BUTTON_NEGATIVE);
+        }
+    }
+}
